6642f4d11bb0857151e6dfe2d9b445830503d6a5
Jennifer Cahalane <cahalane.jennifer@gmail.com>
2016-10-10 19:11:07 -0400

    hooking up tumblr api, rough styles for create pages and rough styles for resume page

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express'),
-    router = express.Router();
+    router = express.Router(),
+    request = require('request'),
+    config = require('../config');
 
 router.get('/', function(req, res) {
     res.render('layouts/about.html', {
@@ -37,14 +39,45 @@ router.get('/resume', function(req, res) {
     });
 });
 
-router.get('/create', function(req, res) {
-    res.render('layouts/create.html', {
-        page: {
-            title: 'Create -- Jennifer Cahalane',
-            description: 'The assorted creations of Jennifer Cahalane -- code, crochet, 3d models, and other art',
-            type: 'create'
+router.get('/create/post/:postId', function(req, res) {
+    request.get('https://api.tumblr.com/v2/blog/cahalanejennifer.tumblr.com/posts?api_key=' + config.TUMBLR_API_KEY + '&id=' + req.params.postId,
+        function (error, response, body) {
+            var data = JSON.parse(body);
+            var post = data.response.posts[Object.keys(data.response.posts)[0]]
+
+            var regex =  /<h2>(.+)<\/h2>/;
+
+            post.title = post.caption.match(regex) ? post.caption.match(regex)[1] : '';
+            post.caption = post.caption.replace(regex, '');
+
+            res.render('layouts/create-post.html', {
+                page: {
+                    title: 'Create -- ' + post.title,
+                    description: 'The assorted creations of Jennifer Cahalane -- code, crochet, 3d models, and other art',
+                    type: 'create'
+                },
+                post: post,
+            });
         }
-    });
+    );
+
+});
+
+router.get('/create', function(req, res) {
+    request.get('https://api.tumblr.com/v2/blog/cahalanejennifer.tumblr.com/posts?api_key=' + config.TUMBLR_API_KEY,
+        function (error, response, body) {
+            var data = JSON.parse(body);
+            // console.log(data.response.posts);
+            res.render('layouts/create.html', {
+                page: {
+                    title: 'Create -- Jennifer Cahalane',
+                    description: 'The assorted creations of Jennifer Cahalane -- code, crochet, 3d models, and other art',
+                    type: 'create'
+                },
+                posts: data.response.posts
+            });
+        }
+    );
 });
 
 router.get('/contact', function(req, res) {
